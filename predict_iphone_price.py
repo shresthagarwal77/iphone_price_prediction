c22f04f037c834dba2d696e2dd2c85ea0e1f9ed1
Shresth Agarwal <68678919+ershresth@users.noreply.github.com>
2021-10-11 21:55:50 +0530

    hh

--- predict_iphone_price.py
+++ predict_iphone_price.py
@@ -2,8 +2,8 @@ import pandas
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 data = pandas.read_csv('iphone_price.csv')
-plt.scatter(data['version'], data['price'])
-plt.show()
+# plt.scatter(data['version'], data['price'])
+# plt.show() #just to show chart
 model = LinearRegression()
 model.fit(data[['version']], data[['price']])
 print(model.predict([[30]]))
